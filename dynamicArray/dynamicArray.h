d1394d07a813eb4b99bf706f7d39a887d9230fd5
cxy <2830206832@qq.com>
2023-12-18 11:18:43 +0800

    [新增]结构体的头文件(.h)和源文件(.c)

---
+++ dynamicArray/dynamicArray.h
@@ -0,0 +1,6 @@
+#ifndef _DYNAMICARRAY_H_
+#define _DYNAMICARRAY_H_
+
+/* 避免文件重复包含 */
+
+#endif
---
+++ dynamicArray/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "dynamicArray.h"
+int main()
+{
+    /*静态数组
+    Q1:不够灵活
+    Q2：可能会浪费空间*/
+
+    int classStu[0];
+   // for(int idx = 0; idx < )
+    return 0;
+}
