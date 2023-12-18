4acc8e64ade6c1f6711297efe167175b3cf17178
cxy <2830206832@qq.com>
2023-12-18 11:41:12 +0800

    [新增]动态数组的接口

--- dynamicArray/dynamicArray.h
+++ dynamicArray/dynamicArray.h
@@ -1,6 +1,40 @@
 #ifndef _DYNAMICARRAY_H_
 #define _DYNAMICARRAY_H_
 
+typedef int ELEMENTTYPE;
 /* 避免文件重复包含 */
+typedef struct dynamicArray
+{
+    ELEMENTTYPE *data;  /* 数组的空间 */
+    int len;            /* 数组的大小 */
+    int capacity;       /* 数组的容量 */
+}dynamicArray;
 
-#endif
+/* API:application program interface */
+/* 动态数组的初始化 */
+int dynamicArrayInit();
+
+/* 动态数组插入数据(默认插到数组的末尾) */
+int dynamicArrayInsertData();
+
+/* 动态数组插入数据，在指定位置插入 */
+int dynamicArrayAppointPosInsertData();
+
+/* 动态数组修改指定位置的数据 */
+int dynamicArrayModifyAppointPosData();
+
+/* 动态数组删除数据 （默认情况下删除最后末尾的数据*/
+int dynamicArrayDeleteData();
+
+/* 动态数组删除指定位置数据 */
+int dynamicArrayDeleteAppointPosDara();
+
+/* 动态数组的销毁 */
+int dynamicArrayDestroy();
+
+/* 获取数组的大小 */
+int dynamicArrayGetSize();
+
+/* 获取数组的容量*/
+int dynamicArrayGetCapacity();
+#endif //_DYNAMICARRAY_H_
