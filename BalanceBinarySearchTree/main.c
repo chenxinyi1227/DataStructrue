b5a183cd574b73dc39b50b2cdfb46bc600688f72
cxy <2830206832@qq.com>
2024-01-30 09:40:49 +0800

    修改测试文件

--- BalanceBinarySearchTree/main.c
+++ BalanceBinarySearchTree/main.c
@@ -24,78 +24,78 @@ int printBasicData(void *arg)
 
 int main()
 {
-    balanceBinarySearchTree * AVL;
-    balanceBinarySearchTreeInit(&AVL, compareBasicDataFunc, printBasicData);
+    AVLTree * AVL;
+    AVLTreeInit(&AVL, compareBasicDataFunc, printBasicData);
 
     int buffer[] = {13, 14, 15, 12, 11, 17, 16, 8, 9, 1};
 
     for (int idx = 0; idx < sizeof(buffer) /sizeof(buffer[0]); idx++)
     {   
-        balanceBinarySearchTreeInsert(AVL, (void *)&buffer[idx]);
+        AVLTreeInsert(AVL, (void *)&buffer[idx]);
     }
     /* 获取二叉搜索树的结点个数 */
     int size = 0;
-    balanceBinarySearchTreeGetNodeSize(AVL, &size);
+    AVLTreeGetNodeSize(AVL, &size);
     printf("size:%d\n", size);
 
     /* 获取二叉搜索树的高度 */
     int height = 0;
-    balanceBinarySearchTreeGetHeight(AVL, &height);
+    AVLTreeGetHeight(AVL, &height);
     printf("height:%d\n", height);
 
     /* 层序遍历 */
-    balanceBinarySearchTreeLevelOrderTraverse(AVL);
+    AVLTreeLevelOrderTraverse(AVL);
     printf("\n");
 
 
     int delVal1 = 11;
-    balanceBinarySearchTreeDelete(AVL, &delVal1);
+    AVLTreeDelete(AVL, &delVal1);
 
     int delVal2 = 12;
-    balanceBinarySearchTreeDelete(AVL, &delVal2);
+    AVLTreeDelete(AVL, &delVal2);
 
     int delVal3 = 13;
-    balanceBinarySearchTreeDelete(AVL, &delVal3);
+    AVLTreeDelete(AVL, &delVal3);
 
 
     /* 获取二叉搜索树的高度 */
     height = 0;
-    balanceBinarySearchTreeGetHeight(AVL, &height);
+    AVLTreeGetHeight(AVL, &height);
     printf("height:%d\n", height);
 
     /* 层序遍历 */
-    balanceBinarySearchTreeLevelOrderTraverse(AVL);
+    AVLTreeLevelOrderTraverse(AVL);
     printf("\n");
 
     /* 销毁 */
-    balanceBinarySearchTreeDestroy(AVL);
+    AVLTreeDestroy(AVL);
 #if 0
     /* 中序遍历 */
-    balanceBinarySearchTreeInOrderTravel(AVL);
+    AVLTreeInOrderTravel(AVL);
     printf("\n");
 
 
     /* 层序遍历 */
-    balanceBinarySearchTreeLevelOrderTravel(AVL);
+    AVLTreeLevelOrderTravel(AVL);
     printf("\n");
 
 
     /* 删除 度为2. 值为56 */
     int delVal = 56;
-    balanceBinarySearchTreeDelete(AVL, &delVal);
+    AVLTreeDelete(AVL, &delVal);
 
     /* 获取二叉搜索树的结点个数 */
     size = 0;
-    balanceBinarySearchTreeGetNodeSize(AVL, &size);
+    AVLTreeGetNodeSize(AVL, &size);
     printf("size:%d\n", size);
 
     /* 获取二叉搜索树的高度 */
     height = 0;
-    balanceBinarySearchTreeGetHeight(AVL, &height);
+    AVLTreeGetHeight(AVL, &height);
     printf("height:%d\n", height);
 
     /* 层序遍历 */
-    balanceBinarySearchTreeLevelOrderTravel(AVL);
+    AVLTreeLevelOrderTravel(AVL);
     printf("\n");
 
 
@@ -103,39 +103,39 @@ int main()
 
     /* 删除 度为1. 值为56 */
     delVal = 7;
-    balanceBinarySearchTreeDelete(AVL, &delVal);
+    AVLTreeDelete(AVL, &delVal);
 
     /* 获取二叉搜索树的结点个数 */
     size = 0;
-    balanceBinarySearchTreeGetNodeSize(AVL, &size);
+    AVLTreeGetNodeSize(AVL, &size);
     printf("size:%d\n", size);
 
     /* 获取二叉搜索树的高度 */
     height = 0;
-    balanceBinarySearchTreeGetHeight(AVL, &height);
+    AVLTreeGetHeight(AVL, &height);
     printf("height:%d\n", height);
 
     /* 层序遍历 */
-    balanceBinarySearchTreeLevelOrderTravel(AVL);
+    AVLTreeLevelOrderTravel(AVL);
     printf("\n");
 
 
     /* 删除 度为0. 值为12 */
     delVal = 12;
-    balanceBinarySearchTreeDelete(AVL, &delVal);
+    AVLTreeDelete(AVL, &delVal);
 
     /* 获取二叉搜索树的结点个数 */
     size = 0;
-    balanceBinarySearchTreeGetNodeSize(AVL, &size);
+    AVLTreeGetNodeSize(AVL, &size);
     printf("size:%d\n", size);
 
     /* 获取二叉搜索树的高度 */
     height = 0;
-    balanceBinarySearchTreeGetHeight(AVL, &height);
+    AVLTreeGetHeight(AVL, &height);
     printf("height:%d\n", height);
 
     /* 层序遍历 */
-    balanceBinarySearchTreeLevelOrderTravel(AVL);
+    AVLTreeLevelOrderTravel(AVL);
     printf("\n");
 #endif
 
